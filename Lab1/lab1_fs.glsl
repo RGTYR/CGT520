c47ebbe332f1104336188b616526a5c7f57536a1
Junjie Luo <luo330@purdue.edu>
2019-09-03 13:14:05 -0400

    update

--- Lab1/lab1_fs.glsl
+++ Lab1/lab1_fs.glsl
@@ -1,11 +1,15 @@
 #version 400
-out vec4 fragcolor; //the output color for this fragment    
-               
+out vec4 fragcolor; //the output color for this fragment
+uniform float time;
+
+// garbage text would let the picture rendered become errors
 void main(void)
 {   
+	float time_ = time;
 
 	//Exercise: try to change the color to red
-	fragcolor = vec4(1.0, 1.0, 1.0, 1.0);
+	fragcolor = vec4(cos(time_), sin(time_), cos(time_ * 2), 1.0);
+
 }
 
 
